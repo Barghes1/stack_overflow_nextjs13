5f1cb57bd722d3b97e4f960d1f92199d1d3e284c
Wei78 <86228418+Wei78@users.noreply.github.com>
2024-08-23 17:23:46 +0300

    remove a tags props

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -30,17 +30,18 @@ const page = async ({ params, searchParams }: URLProps) => {
             <div className="mt-10 flex w-full flex-col gap-6">
                 {result.questions.length > 0 ?
                 result.questions.map((question: IQuestion) => (
-                    <QuestionCard 
-                        key={question._id}
-                        _id={question._id}
-                        title={question.title}
-                        tags={question.tags}
-                        author={question.author}
-                        upvotes={question.upvotes}
-                        views={question.views}
-                        answers={question.answers}
-                        createdAt={question.createdAt}
-                    />
+                    // <QuestionCard 
+                    //     key={question._id}
+                    //     _id={question._id}
+                    //     title={question.title}
+                    //     tags={question.tags}
+                    //     author={question.author}
+                    //     upvotes={question.upvotes}
+                    //     views={question.views}
+                    //     answers={question.answers}
+                    //     createdAt={question.createdAt}
+                    // />
+                    <>Tag</>
                 )) : <NoResult 
                         title="There's no tag questions to show" 
                         description="Be the first to break the silence! Ask a Question
